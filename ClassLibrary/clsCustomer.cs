e56f3cbcde98266b4fd7ea41c3db3e5fcf476558
James Mboma <42808235+mboma99@users.noreply.github.com>
2022-02-16 18:14:03 +0000

    Created tblCustomer test cases and properties

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
     {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string CustomerDetails { get; set; }
+        public double AccountBalance { get; set; }
+        public bool PendingOrder { get; set; }
+        
     }
 }
