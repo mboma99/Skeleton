5ba36e5bb065ae056b983c6778626b9b7c900c55
Angelo Felix Van-Dunem Narciso <P2577612@my365.dmu.ac.uk>
2022-02-16 21:49:08 +0000

    Added testing for orders class

---
+++ Testing2/tstOrders.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstOrders
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsOrders AnOrder = new clsOrders();
+            Assert.IsNotNull(AnOrder);
+        }
+    }
+}
