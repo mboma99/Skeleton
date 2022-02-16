769fba249b9390bc7d010df4c4e2cf16efc1f76f
James Mboma <42808235+mboma99@users.noreply.github.com>
2022-02-16 14:41:28 +0000

    Added testing for Customer class

---
+++ Testing1/tstCustomer.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //test to see that it exists
+            Assert.IsNotNull(ACustomer);
+        }
+    }
+}
