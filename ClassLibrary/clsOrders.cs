143128516a8fc56f16718b0e438e37fb5f3b780c
Angelo Felix Van-Dunem Narciso <P2577612@my365.dmu.ac.uk>
2022-03-09 07:10:43 +0000

    Created the Find Method

--- ClassLibrary/clsOrders.cs
+++ ClassLibrary/clsOrders.cs
@@ -1,13 +1,96 @@
-﻿namespace ClassLibrary
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary
 {
     public class clsOrders
     {
-        public int OrderID { get; set; }
-        public int CustomerID { get; set; }
-        public bool Approval { get; set; }
-        public bool SaleApplied { get; set; }
-        public string OrderStatus { get; set; }
+        
+        
+        private Int32 mOrderID;
+        public int OrderID
+        {
+            get 
+            {
+                return mOrderID
+            
+            }
 
+            set
+            {
+                mOrderID = value;
+            }
+        }
 
+        private Int32 mCustomerID;
+        public int CustomerID
+        {
+            get 
+            {
+                return mCustomerID
+            
+            }
+
+            set
+            {
+                mCustomerID = value;
+            }
+        }
+        
+        private Boolean mApproval;
+        public bool Approval
+        { 
+            get 
+            {
+                //return the private data
+                return mApproval;
+            }
+            set 
+            {
+                //set the value of the private data member
+                mApproval = value;
+            } 
+        }
+        
+        private Boolean mSaleApplied;
+        public bool SaleApplied
+        { 
+            get 
+            {
+                //return the private data
+                return mSaleApplied;
+            }
+            set 
+            {
+                //set the value of the private data member
+                mSaleApplied = value;
+            } 
+        }
+        
+        private string mOrderStatus;
+        public string OrderStatus
+        { 
+            get 
+            {
+                //return the private data
+                return mOrderStatus;
+            }
+            set 
+            {
+                //set the value of the private data member
+                mOrderStatus = value;
+            } 
+        }
+
+        public bool Find(int OrderID)
+        {
+            mOrderID = 3;
+            mCustomerID = 3;
+            mApproval = true;
+            mSaleApplied = true;
+            mOrderStatus = "En Route";
+            return true;
+        }
     }
 }
--- Testing2/tstOrders.cs
+++ Testing2/tstOrders.cs
@@ -74,5 +74,151 @@ namespace Testing2
             
             Assert.AreEqual(Acustomer.OrderStatus, TestData);
         }
+
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            clsOrders AnOrder= new clsOrders();
+
+            Boolean Found = false;
+
+            Int32 OrderIDNO = 3;
+
+            Found = AnOrder.Find(OrderIDNO);
+
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void TestOrderIDNOFound()
+        {
+            clsOrders AnOrder = new clsOrders();
+
+            Boolean Found = false;
+
+            Boolean OK = true;
+
+            Int32 OrderIDNO = 3;
+
+            Found = AnOrder.Find(OrderIDNO);
+
+            if(AnOrder.OrderIDNO != 3)
+            {
+                OK = false;
+            }
+
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void OrderIDFound()
+        {
+            //create an instance of the class we want to create
+            clsOrders AnOrder = new clsOrders();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 OrderID = 3;
+            //invoke the method
+            Found = AnOrder.Find(OrderID);
+            //check the property
+            if (AnOrder.OrderID != 3)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void CustomerIDFound()
+        {
+            //create an instance of the class we want to create
+            clsOrders AnOrder = new clsOrders();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 OrderID = 3;
+            //invoke the method
+            Found = AnOrder.Find(OrderID);
+            //check the property
+            if (AnOrder.CustomerID != 3)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void HasApprovalFound()
+        {
+            //create an instance of the class we want to create
+            clsOrders AnOrder = new clsOrders();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 OrderID = 3;
+            //invoke the method
+            Found = AnOrder.Find(OrderID);
+            //check the property
+            if (AnOrder.HasApproval != true)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void HasSaleAppliedFound()
+        {
+            //create an instance of the class we want to create
+            clsOrders AnOrder = new clsOrders();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 OrderID = 3;
+            //invoke the method
+            Found = AnOrder.Find(OrderID);
+            //check the property
+            if (AnOrder.HasSaleApplied != true)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void OrderStatusFound()
+        {
+            //create an instance of the class we want to create
+            clsOrders AnOrder = new clsOrders();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 OrderID = 3;
+            //invoke the method
+            Found = AnOrderLine.Find(OrderID);
+            //check the property
+            if (AnOrderLine.OrderStatus != "En Route")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
     }
 }
