122bfb04e2b81d96ecc32ae04e3a9f79eabb7e52
Angelo Felix Van-Dunem Narciso <P2577612@my365.dmu.ac.uk>
2022-03-11 18:49:37 +0000

    Erros fixe (still have work to do)

--- Testing2/tstOrders.cs
+++ Testing2/tstOrders.cs
@@ -1,11 +1,12 @@
-﻿using ClassLibrary;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Testing2
 {
-    [TestClass]
-    public class tstOrders
+    class tstOrders
     {
         [TestMethod]
         public void InstanceOK()
@@ -17,68 +18,68 @@ namespace Testing2
         [TestMethod]
         public void OrderIDAdded()
         {
-            
+
             clsOrders AnOrder = new clsOrders();
-            
+
             int TestData = 3;
             AnOrder.OrderID = TestData;
-            
+
             Assert.AreEqual(AnOrder.OrderID, TestData);
         }
 
         [TestMethod]
         public void CustomerIDAdded()
         {
-            
+
             clsOrders AnOrder = new clsOrders();
-            
+
             int TestData = 3;
             AnOrder.CustomerID = TestData;
-            
+
             Assert.AreEqual(AnOrder.CustomerID, TestData);
         }
-        
+
         [TestMethod]
         public void HasApproval()
         {
-            
+
             clsOrders AnOrder = new clsOrders();
-            
+
             Boolean TestData = true;
             AnOrder.Approval = TestData;
-            
+
             Assert.AreEqual(AnOrder.Approval, TestData);
         }
-        
+
         [TestMethod]
         public void HasSaleApplied()
         {
-            
+
             clsOrders AnOrder = new clsOrders();
-             
+
             Boolean TestData = true;
             AnOrder.SaleApplied = TestData;
-            
+
             Assert.AreEqual(AnOrder.SaleApplied, TestData);
         }
 
         [TestMethod]
         public void OrderStatusAddedOK()
         {
-            
+
             clsOrders Acustomer = new clsOrders();
-            
+
             String TestData = "En Route";
-            
+
             Acustomer.OrderStatus = TestData;
-            
+
             Assert.AreEqual(Acustomer.OrderStatus, TestData);
         }
 
         [TestMethod]
         public void FindMethodOK()
         {
-            clsOrders AnOrder= new clsOrders();
+            clsOrders AnOrder = new clsOrders();
 
             Boolean Found = false;
 
@@ -102,7 +103,7 @@ namespace Testing2
 
             Found = AnOrder.Find(OrderIDNO);
 
-            if(AnOrder.OrderIDNO != 3)
+            if (AnOrder.OrderIDNO != 3)
             {
                 OK = false;
             }
@@ -219,6 +220,5 @@ namespace Testing2
             //test to see that the result is correct
             Assert.IsTrue(OK);
         }
-
     }
 }
