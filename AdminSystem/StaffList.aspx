3b2ad70a51d20d754caeed4b5ded66c30eda7391
Reginold-P <P2589849@my365.dmu.ac.uk>
2022-03-21 12:30:12 +0000

    created collection class and properties for Staff - Week 26

--- AdminSystem/StaffList.aspx
+++ AdminSystem/StaffList.aspx
@@ -10,6 +10,7 @@
     <form id="form1" runat="server">
         <div>
         </div>
+        <asp:ListBox ID="lstStaffList" runat="server" Height="566px" Width="571px"></asp:ListBox>
     </form>
 </body>
 </html>
--- AdminSystem/StaffList.aspx.cs
+++ AdminSystem/StaffList.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,27 @@ public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (IsPostBack == false)
+        {
+            DisplayStaffList();
+        }
+    }
 
+    void DisplayStaffList()
+    {
+        //create an instance of the Staff Collection
+        clsStaffCollection StaffList = new clsStaffCollection();
+        //set the data source to list of staff in the collection
+        lstStaffList.DataSource = StaffList.StaffList;
+        //set the name of the primary key
+        lstStaffList.DataValueField = "StaffID";
+        //set the data field to display
+        lstStaffList.DataTextField = "Name";
+        //lstStaffList.DataTextField = "DOB";
+        //lstStaffList.DataTextField = "Address";
+        //lstStaffList.DataTextField = "Salary";
+        //lstStaffList.DataTextField = "IsActive";
+        //bind the data to the list
+        lstStaffList.DataBind();
     }
 }
